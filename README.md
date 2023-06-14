3fff598bf19504fde5b15652f6203a275b13af3d
Ahmed Samir <ahmedsamir96m@gmail.com>
2023-06-14 14:53:07 +0300

    working on nagwa

---
+++ README.md
@@ -0,0 +1,29 @@
+# About This Project
+
+This is a task project sent by HR Yara from Nagwa [Nagwa](https://nagwa.com).
+
+## To start this project
+
+After clone the project rep, first run the following script in client and server directories
+
+### `npm install`
+
+Installing all required dependencies in the package.json files
+
+Then in the server dir, run the following
+
+### `npm run dev`
+
+Runs the server side in the development mode on port 3001 .\
+
+And in the client dir, run the following
+
+### `npm start`
+
+Runs the server side in the development mode on port 3000 .\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+# To use the app 
+1- choose an answer for the current question
+2- hit the 'Next' button to go forward to the next question
+3- after answering all of the question the screen will be update with your score
--- server/api/ranks.js
+++ server/api/ranks.js
@@ -1,38 +1,38 @@
-import express from 'express'
-import wordsJSON from '../TestData.json' assert { type: "json" };
+import express from "express";
+import wordsJSON from "../TestData.json" assert { type: "json" };
 
-const router = express.Router()
+const router = express.Router();
 
-let scoresList = wordsJSON.scoresList
+let scoresList = wordsJSON.scoresList;
 
 // FUNCTION TO CALC RANK PERCENTILE
 const calcPercentile = async (answer) => {
-  const userScore = answer.answer
-  let belowScoresOccur = 0
+  const userScore = answer?.answer;
+  let belowScoresOccur = 0;
   for (let index = 0; index < scoresList.length; index++) {
     if (userScore > scoresList[index]) {
-      belowScoresOccur += 1
+      belowScoresOccur += 1;
     }
   }
   let finalScore;
-  finalScore = (belowScoresOccur / scoresList.length * 100).toFixed(2) % 1 > 0 ?
-    +(belowScoresOccur / scoresList.length * 100).toFixed(2)
-    :
-    Math.round((belowScoresOccur / scoresList.length * 100).toFixed(2))
-
+  finalScore =
+    ((belowScoresOccur / scoresList.length) * 100).toFixed(2) % 1 > 0
+      ? +((belowScoresOccur / scoresList.length) * 100).toFixed(2)
+      : Math.round(((belowScoresOccur / scoresList.length) * 100).toFixed(2));
+  console.log(userScore);
   return {
-    rankPercentile: finalScore
-  }
-}
+    rankPercentile: finalScore,
+  };
+};
 
-router.get('/ranks', (req, res) => {
-  res.send('ranks')
-})
+router.get("/ranks", (req, res) => {
+  res.send("ranks");
+});
 
-router.post('/ranks', async (req, res) => {
-  let answer = req.body
-  let rankPercentile = await calcPercentile(answer)
-  res.status(200).send(rankPercentile)
-})
+router.post("/ranks", async (req, res) => {
+  let answer = req.body;
+  let rankPercentile = await calcPercentile(answer);
+  res.status(200).send(rankPercentile);
+});
 
-export default router
+export default router;
